11ff554822fca4292bd94bd7268800e4780a60be
Bruna Ferreira <bferreira.slv@gmail.com>
2023-11-22 22:12:14 -0300

    Apply poetry tool

--- back-end/src/main.py
+++ back-end/src/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def index():
     return{"message": "hello daily!"}
+
+
+def start():
+    uvicorn.run("src.main:app", host="127.0.0.1", reload=True)
