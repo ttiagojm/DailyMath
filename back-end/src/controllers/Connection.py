a8bd855b0d975ea67cf26849204c29d65b305b51
Finalshare90 <77998939+Finalshare90@users.noreply.github.com>
2023-11-28 00:47:44 -0300

    Update Connection.py

--- back-end/src/controllers/Connection.py
+++ back-end/src/controllers/Connection.py
@@ -1,7 +1,6 @@
 class Connection:
 
      def __init__ (self, id:int, isMultiple:bool, token:int):
-         print("New connection")
          self.id = id
          self.isMultiple = isMultiple
          self.token = token
