0fa872fa1ecde8e8ee58c3ee105bb73e90959bfc
ttiagojm <tiagodeha@protonmail.com>
2024-04-06 14:46:04 +0100

    Database init in DailyServlet

--- back-end/src/main/java/com/math/dailymath/DailyServlet.java
+++ back-end/src/main/java/com/math/dailymath/DailyServlet.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @WebServlet(name="DailyServlet", urlPatterns = "/daily")
 public class DailyServlet extends HttpServlet {
@@ -17,16 +20,40 @@ public class DailyServlet extends HttpServlet {
     // This variables are shared with all requests (thread safety can be a problem)
     private final Gson gson = new Gson();
     private final ServiceExercise exercise = new ServiceExercise();
+    Connection conn = null;
+
+    /**
+     * Initialization of Servlet where it's initialize the database
+     * @throws ServletException
+     */
+    public void init() throws ServletException {
+        String database = System.getenv("POSTGRES_DB");
+        String user = System.getenv("POSTGRES_USER");
+        String passwd = System.getenv("POSTGRES_PASSWORD");
+
+        try {
+            Class.forName("org.postgresql.Driver");
+
+            this.conn = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/"+database,
+                    user,
+                    passwd
+            );
+            System.out.print("CREATED DATABASE");
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
         PrintWriter printWriter = resp.getWriter();
 
         // Thread Safety guaranteed
         synchronized (this) {
-            String exerciseString = gson.toJson(exercise.getExercise());
+            String exerciseString = gson.toJson(exercise.getExercise(conn));
             printWriter.print(exerciseString);
             printWriter.close();
         }
--- back-end/src/main/java/com/math/dailymath/services/ServiceExercise.java
+++ back-end/src/main/java/com/math/dailymath/services/ServiceExercise.java
@@ -1,22 +1,17 @@
 package com.math.dailymath.services;
 
-import com.math.dailymath.models.Database;
 import com.math.dailymath.models.Exercise;
 
+import java.sql.Connection;
 import java.time.LocalDate;
 
 public class ServiceExercise {
-    private final Database db;
 
     // Variables to keep daily exercise up to date
     private static LocalDate today = LocalDate.now();
     private static Exercise exercise = null;
 
-    public ServiceExercise(){
-        this.db = Database.getDatabase();
-    }
-
-    public Exercise getExercise(){
+    public Exercise getExercise(Connection conn){
         // Verify if the exercise needs to be changed
         if(exercise == null || today.isBefore(LocalDate.now())){
             // TODO Call Database instance to get all exercises not randomized
